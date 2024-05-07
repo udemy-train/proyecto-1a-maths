00c5979918b00112c1053b9e1d8ce0b9e7590d58
david.duarte <david.duarte@coppel.com>
2024-05-07 11:02:41 -0600

    Primer version module

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+    /**
+     * Suma de dos numeros
+     * @param {*} n1 
+     * @param {*} n2 
+     * @returns 
+     */
+    suma: function (n1, n2) {
+        return n1 + n2;
+    },
+
+    /**
+     * Resta de dos numeros
+     * @param {*} n1 
+     * @param {*} n2 
+     * @returns 
+     */
+    resta: function (n1, n2) {
+        return n1 - n2;
+    },
+
+    /**
+     * multiplicacion de dos numeros
+     * @param {*} n1 
+     * @param {*} n2 
+     * @returns 
+     */
+    multiplicacion: function (n1, n2) {
+        return n1 * n2;
+    },
+
+    /**
+     * division de dos numeros
+     * @param {*} n1 
+     * @param {*} n2 
+     * @returns 
+     */
+    division: function (n1, n2) {
+        return n1 / n2;
+    },
+}
---
+++ tests.js
@@ -0,0 +1,6 @@
+import { suma, resta, multiplicacion, division } from './index.js';
+
+console.log(suma(4,5));
+console.log(resta(4,5));
+console.log(multiplicacion(4,5));
+console.log(division(4,5));
